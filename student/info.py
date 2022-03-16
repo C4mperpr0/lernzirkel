3046c7805ab84337da33186b1fc6a422d5729745
c4mperpr0 <carlbellgardt@gmail.com>
2022-03-16 14:04:22 +0100

    added student/info WIP do not use

--- student/info.py
+++ student/info.py
@@ -12,11 +12,11 @@ with open('./templateColors.json', 'r') as file:
     global colorThemes
     colorThemes = json.loads(file.read())
 
-studentInfo = Blueprint("list", __name__, static_folder="static", template_folder="templates/student")
+studentInfo = Blueprint("info", __name__, static_folder="static", template_folder="templates/student")
 
 
 @studentInfo.route('/<string:studentname>', methods=['GET', 'POST'])
-def studentList_():
+def studentInfo_():
     print(studentname)
     return render_template('info.html',
                            **colorThemes['default'])
